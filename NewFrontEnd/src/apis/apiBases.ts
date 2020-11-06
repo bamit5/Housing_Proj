c867f9bce1b0a6c95cd99555f1f3e5ea3a0c5196
Amit Bar <45904412+bamit5@users.noreply.github.com>
2020-11-05 20:47:33 -0800

    Update apiBases.ts

--- NewFrontEnd/src/apis/apiBases.ts
+++ NewFrontEnd/src/apis/apiBases.ts
@@ -6,9 +6,9 @@ export const googleMapsAPI = axios.create({
 
 // TODO add a middleware to check for an error and throw an error if there is one. Otherwise, extract only the data
 export const backendAPI = axios.create({
-  baseURL: 'http://3.134.77.241:3001/',
+  baseURL: 'https://3.134.77.241:3001/',
 });
 
 export const backendAPIFake = axios.create({
-  baseURL: 'http://3.134.77.241:3001/',
+  baseURL: 'https://3.134.77.241:3001/',
 });
